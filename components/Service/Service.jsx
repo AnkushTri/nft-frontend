7e980e8e2aee1f5a73161a5e67c9cf645d9f2314
Ankush kumar <kumarankush13701@gmail.com>
2023-03-17 07:41:13 +0530

    17-03-23tillCategory

--- components/Service/Service.jsx
+++ components/Service/Service.jsx
@@ -1,8 +1,57 @@
 import React from 'react';
+import Image from 'next/image';
+
+//Internal imports 
+import Style from './Service.module.css';
+import images from '../../img';
+
 
 const Service = () => {
   return (
-    <div>Service</div>
+    <div className={Style.service}>
+      <div className={Style.service_box}>
+        <div className={Style.service_box_item}>
+          <Image src={images.service1} alt="Filter and Discover" width={100} height={100}/>
+          <p className={Style.service_box_item_step}>
+            <span>step 1</span>
+          </p>
+          <h3>Filter and Discover</h3>
+          <p>
+            Connect with wallet, discover , buy NFTs, sell your NFts and earn money
+          </p>
+        </div>
+        <div className={Style.service_box_item}>
+          <Image src={images.service2} alt="Filter and Discover" />
+          <p className={Style.service_box_item_step}>
+            <span>step 1</span>
+          </p>
+          <h3>Filter and Discover</h3>
+          <p>
+            Connect with wallet, discover , buy NFTs, sell your NFts and earn money
+          </p>
+        </div>
+        <div className={Style.service_box_item}>
+          <Image src={images.service3} alt="Connect Wallet" />
+          <p className={Style.service_box_item_step}>
+            <span>step 1</span>
+          </p>
+          <h3>Connect wallet</h3>
+          <p>
+            Connect with wallet, discover , buy NFTs, sell your NFts and earn money
+          </p>
+        </div>
+        <div className={Style.service_box_item}>
+          <Image src={images.service4} alt="Start trading" />
+          <p className={Style.service_box_item_step}>
+            <span>step 1</span>
+          </p>
+          <h3>Start trading</h3>
+          <p>
+            Connect with wallet, discover , buy NFTs, sell your NFts and earn money
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import { HeroSection,Service,BigNFTSlider, Subscribe,Title,Category } from '@/components/componentsindex';
+import React from 'react';
+
+//internal import
+import Style from "../styles/index.module.css";
 
 const index = () => {
   return (
-    <div>
-      
+    <div className={Style.homePage}>
+      <HeroSection/>
+      <Service/>
+      <BigNFTSlider/>
+      <Subscribe/>
+      <Title heading="Browse by Category" 
+      paragraph="Explore the NFTs in the most featured categories."
+      />
+      <Category/>
     </div>
   )
 }
 
-export default index
+export default index;
